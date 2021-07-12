1f8cfacd3ce1764b0902f23589e5246d86e64878
tkobierecki <tomasz@vortexiot.com>
2021-07-12 20:35:42 +0100

    fix build issues

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import Link from 'Next/Link';
-import styled from 'styled-components';
+import React from "react";
+// import Link from "next/Link";
+import styled from "styled-components";
 
-import SideNavElement from './SideNavElement';
+import SideNavElement from "./SideNavElement";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding-top: 40px;
   padding-left: 40px;
-  background: #F1F4F9;
+  background: #f1f4f9;
 `;
 
 const SideNav = () => {
   return (
     <Container>
-      <SideNavElement destination={'/dashboard'} description={'mój panel'}/>
-      <SideNavElement destination={'/dashboard/mydata'} description={'moje dane'}/>
-      <SideNavElement destination={'/dashboard/clients'} description={'klienci'}/>
-      <SideNavElement destination={'/dashboard/messages'} description={'wiadomosći'}/>
+      <SideNavElement destination={"/dashboard"} description={"mój panel"} />
+      <SideNavElement
+        destination={"/dashboard/mydata"}
+        description={"moje dane"}
+      />
+      <SideNavElement
+        destination={"/dashboard/clients"}
+        description={"klienci"}
+      />
+      <SideNavElement
+        destination={"/dashboard/messages"}
+        description={"wiadomosći"}
+      />
     </Container>
   );
-}
+};
 
 export default SideNav;
--- components/SideNavElement.tsx
+++ components/SideNavElement.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Link from 'Next/Link';
-import styled from 'styled-components';
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
 
 const StyledLink = styled.a`
-  color: ${(props) => props.selected ?'#0F35B7' : '#272E39'};
+  color: ${(props) => (props.selected ? "#0F35B7" : "#272E39")};
   font-family: Roboto;
   margin-bottom: 20px;
   text-transform: uppercase;
 `;
 
-
-const SideNavElement = ({description, destination}) => {
+const SideNavElement = ({ description, destination }) => {
   return (
     <Link href={destination}>
       <StyledLink>{description}</StyledLink>

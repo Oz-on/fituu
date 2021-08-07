7e3f25b4d4e0a0636e7d34c10464c1e1f095d061
Oskar Domingos <oskarad.2000@gmail.com>
2021-08-07 12:53:20 +0200

    stories fixed

--- components/organisms/DataCard/index.stories.ts
+++ components/organisms/DataCard/index.stories.ts
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Dashboard from './';
+import DataCard from ".";
 
 export default {
-  title: 'Components/Pages/Dashboard',
-  component: Dashboard,
-}
+  component: DataCard,
+  title: "Components/Organisms/DataCard",
+};
 
-const Template = (args) => <Dashboard {...args} />;
+const Template = (args) => <DataCard {...args} />
 
 export const Default = Template.bind({});
 Default.args = {
-  session: {},
-  email: 'oskarad.2000@gmail.com',
-  profilePictureUrl: 'https://gravatar.com/avatar/d496b8bf36092d7d0796cf0cb1de8b27',
   userData: {
     firstName: 'Janina',
     lastName: 'Kowalska',
@@ -27,4 +24,4 @@ Default.args = {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus at enim id sit at. Placerat pharetra sed ligula id quam aliquam aliquam. Eget in eget massa vitae diam nibh consequat facilisi posuere. Varius nunc tempus leo at sagittis elementum aliquam rhoncus. A laoreet amet risus risus. Arcu, in rhoncus, curabitur iaculis tincidunt cursus. Sed tellus dui volutpat velit. Nec amet egestas sit pulvinar at eu. Vehicula ultricies nunc, pharetra risus pulvinar diam id odio. Odio volutpat dignissim sagittis sit quam. Bibendum accumsan quis placerat.',
     type: 'Trener Personalny'
   },
-};
+}
